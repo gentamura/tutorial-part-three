378a46a0453a96cbbc36bd8844ba4c5bdfe0d4c0
Gen Tamura <gen.tamura84@gmail.com>
2018-08-26 18:19:19 +0900

    Updated contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,8 +3,25 @@ import React from "react";
 export default () => (
   <div>
     <h1>I'd love to talk! Email me at the address below</h1>
-    <p>
-      <a href="mailto:me@example.com">me@example.com</a>
-    </p>
+    <form name="contact" method="POST" netlify>
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </div>
 );
